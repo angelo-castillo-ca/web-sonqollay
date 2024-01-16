4fb51947ae36a4157597af137afb267b4f9e0091
angelo-castillo-ca <123595143+angelo-castillo-ca@users.noreply.github.com>
2024-01-15 22:17:27 -0500

    sonqollay

---
+++ SQL/sonqollay.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS sonqollay;
+CREATE DATABASE IF NOT EXISTS sonqollay;
+USE sonqollay;
+CREATE TABLE liderazgo(
+    id MEDIUMINT NOT NULL PRIMARY KEY,
+    pregunta varchar(300) not null,
+    respuesta_correcta varchar(300) not null,
+    respuesta1 varchar(300) not null,
+    respuesta2 varchar(300) not null,
+    respuesta3 varchar(300) not null,
+    respuesta4 varchar(300) not null
+);
